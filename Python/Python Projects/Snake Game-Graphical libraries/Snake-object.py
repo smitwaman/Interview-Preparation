257747b1891ee1438e8c77720d59c095e8119fe5
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-04-02 12:45:59 +0530

    Update Snake-object.py

--- Python/Python Projects/Snake Game-Graphical libraries/Snake-object.py
+++ Python/Python Projects/Snake Game-Graphical libraries/Snake-object.py
@@ -1,1 +1,63 @@
+import pygame
+import random
+
+CELL_SIZE = 20
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+
+class Snake:
+    def __init__(self):
+        self.length = 1
+        self.positions = [((SCREEN_WIDTH // 2), (SCREEN_HEIGHT // 2))]
+        self.direction = random.choice([pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT])
+        self.color = GREEN
+        self.score = 0
+
+    def get_head_position(self):
+        return self.positions[0]
+
+    def turn(self, point):
+        if self.length > 1 and (point[0]*-1, point[1]*-1) == self.direction:
+            return
+        else:
+            self.direction = point
+
+    def move(self):
+        cur = self.get_head_position()
+        x, y = self.direction
+        new = (((cur[0] + (x*CELL_SIZE)) % SCREEN_WIDTH), (cur[1] + (y*CELL_SIZE)) % SCREEN_HEIGHT)
+        if len(self.positions) > 2 and new in self.positions[2:]:
+            self.reset()
+        else:
+            self.positions.insert(0, new)
+            if len(self.positions) > self.length:
+                self.positions.pop()
+
+    def reset(self):
+        self.length = 1
+        self.positions = [((SCREEN_WIDTH // 2), (SCREEN_HEIGHT // 2))]
+        self.direction = random.choice([pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT])
+        self.score = 0
+
+    def draw(self, surface):
+        for p in self.positions:
+            r = pygame.Rect((p[0], p[1]), (CELL_SIZE, CELL_SIZE))
+            pygame.draw.rect(surface, self.color, r)
+            pygame.draw.rect(surface, BLACK, r, 1)
+
+    def handle_keys(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                elif event.key == pygame.K_UP:
+                    self.turn((0, -1))
+                elif event.key == pygame.K_DOWN:
+                    self.turn((0, 1))
+                elif event.key == pygame.K_LEFT:
+                    self.turn((-1, 0))
+                elif event.key == pygame.K_RIGHT:
+                    self.turn((1, 0))
 
