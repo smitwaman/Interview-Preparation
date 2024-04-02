148eef2618e77c20a68be79bcb1a246e7a5a78b8
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-04-02 10:23:47 +0530

    Create usercontroller.java

---
+++ Java/Java projects/Microservice using springboot/User/usercontroller.java
@@ -0,0 +1,22 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    // Other endpoints as needed
+                           }
